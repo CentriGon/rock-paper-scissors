e8a1f05f4e8dfd7d5ff46c4c1847569723d40269
Noor Nile <aarondunkin123@gmail.com>
2023-01-20 18:07:23 -0500

    created method for determing who wins match

--- script.js
+++ script.js
@@ -12,6 +12,43 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function playerChoice() {
+    let p = 0;
+        return (prompt("Please enter in rock, paper, or scissors"));
+}
+
+function playRound() {
+    let userInput = playerChoice().toLowerCase();
+    let compInput = getComputerChoice().toLowerCase();
+    console.log(userInput);
+    console.log(compInput);
+
+    if ((userInput == "rock" && compInput == "rock") || ((userInput == "paper" && compInput == "paper")) || ((userInput == "scissor" && compInput == "scissor"))) {
+        console.log("It is a tie!");
+    }
+
+    if ((userInput == "rock") && (compInput == "paper")) {
+        console.log("you lose, paper beats rock!");
+    }
+    else if ((userInput == "paper") && (compInput == "rock")) {
+        console.log("you win, paper beats rock!");
+    }
+    else if ((userInput == "paper") && (compInput == "scissor")) {
+        console.log("you lose, scissor beats paper!");
+    }
+    else if ((userInput == "scissor") && (compInput == "paper")) {
+        console.log("you win, scissor beats paper!");
+    }
+    else if ((userInput == "rock") && (compInput == "scissor")) {
+        console.log("you win, rock beats scissor!");
+    }
+    else if ((userInput == "scissor") && (compInput == "rock")) {
+        console.log("you lose, rock beats scissor!");
+    }
+
+}
+
+playRound();
+
+
+
